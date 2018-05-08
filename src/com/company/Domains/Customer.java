d26e449f4daae549a64d0bd9bf19adece2bbc5ef
Rick <rick.van.schijndel@hotmail.com>
2018-05-08 11:02:08 +0200

    Add @entity compatibility

--- src/com/company/Domains/Customer.java
+++ src/com/company/Domains/Customer.java
@@ -1,8 +1,42 @@
 package com.company.Domains;
 
+import javax.persistence.*;
+
+/**
+ * Customer class, contains mostly contact information.
+ */
+@Entity
 public class Customer {
+
+    @Id     @GeneratedValue
     private Long id;
+
     private String firstname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private String surname;
     private String postcode;
     private String address;
@@ -10,4 +44,42 @@ public class Customer {
     private String country;
 
     private String email;
+
+
+    /**
+     * DO NOT USE!
+     */
+    public Customer() {}   // Blank constructor to ensure JPA compatibility.
+
+    /**
+     * Create a new customer with only essential information.
+     * @param firstname
+     * @param surname
+     * @param email
+     */
+    public Customer(String firstname, String surname, String email) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.email = email;
+    }
+
+    /**
+     * Create a new customer with all possible information.
+     * @param firstname
+     * @param surname
+     * @param postcode
+     * @param address
+     * @param city
+     * @param country
+     * @param email
+     */
+    public Customer(String firstname, String surname, String postcode, String address, String city, String country, String email) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.postcode = postcode;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.email = email;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
 
+import com.company.Domains.Customer;
+
+import javax.persistence.*;
+import java.util.Scanner;
+
 public class Main {
 
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("bowshop");
+    private static EntityManager em = emf.createEntityManager();
+    private static EntityTransaction et = null;
+
     public static void main(String[] args) {
-	// write your code here
+        testTransaction();
+    }
+
+    public static void testTransaction() {
+        Scanner scanner = new Scanner(System.in);
+
+        show("First we'll need to create a new customer");
+        show("Please input this person's FIRST name:");
+        String firstname = scanner.nextLine();
+        show("Great, now please input " + firstname + "'s LAST name:");
+        String surname = scanner.nextLine();
+        show("Almost done creating " + firstname + " " + surname + ". Last step; please input this customer's email address:");
+        String email = scanner.nextLine();
+
+        Customer newCustomer = new Customer(firstname, surname, email);
+        show(newCustomer.getFirstname() + " " + newCustomer.getSurname() + " has been created!");
+
+        show("Now trying to insert this guy into our database.");
+
+        try {
+            // Get transaction.
+            et = em.getTransaction();
+
+            // Start the transaction.
+            et.begin();
+
+            // Sync the new customer with database.
+            em.persist(newCustomer);
+
+            // Finally commit changes
+            et.commit();
+        } catch (Exception ex) {
+            throw ex; // Just make it the user's problem.
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void show(String message) {
+        System.out.println(message);
     }
 }
