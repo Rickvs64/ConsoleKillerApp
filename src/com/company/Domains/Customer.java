4b77b497fa004d400e18f74184ea9ce84e7183a8
Rick <rick.van.schijndel@hotmail.com>
2018-05-08 09:50:32 +0200

    Add Customer class

--- src/com/company/Domains/Customer.java
+++ src/com/company/Domains/Customer.java
@@ -1,4 +1,13 @@
 package com.company.Domains;
 
 public class Customer {
+    private Long id;
+    private String firstname;
+    private String surname;
+    private String postcode;
+    private String address;
+    private String city;
+    private String country;
+
+    private String email;
 }
